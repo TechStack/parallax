3bcf11df7e1154b23af4503f8c4dc7f972b5fc3f
TechStack <TechStackLP@gmail.com>
2019-01-18 06:30:12 -0500

    opps didnt save editing out uneeded code :)

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -5,16 +5,7 @@ using UnityEngine;
 public class Spin : MonoBehaviour
 {
 
-   public float speed = 1;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   public float speed = 1; 
     void Update()
     {
         transform.Rotate(new Vector3(0, 1, 0), speed * Time.deltaTime);
